4865d48ad674ba98ed61336813c180bedf6aaf88
Quan Nguyen <nguyenlephuongquan@gmail.com>
2025-03-09 15:39:51 +1300

    Commit message describing local main changes

--- BusTrackerBackend/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
+++ BusTrackerBackend/Contracts/obj/Debug/net8.0/Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2798af4c55a4741d00e6ea6d59505fc5ff891e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a523ec2aed79cffa6d57379f74728e6a3a16a5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BusTrackerBackend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ BusTrackerBackend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2798af4c55a4741d00e6ea6d59505fc5ff891e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a523ec2aed79cffa6d57379f74728e6a3a16a5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
